ab0972dee2a63bb1e44b4a248ad35b5a804e002d
MorizSchildorfer <moriz.schildorfer@gmail.com>
2026-04-18 11:38:23 +0200

    use the same line

--- cogs/generic.py
+++ cogs/generic.py
@@ -1002,7 +1002,7 @@ Command Checklist
 
             except asyncio.TimeoutError:
                 try:
-                    stampEmbedmsg = await self.stamp(ctx, startTime, author, embed=stampEmbed, embedMsg=stampEmbedmsg)
+                    stampEmbedmsg = await ctx.invoke(self.timer.get_command('stamp'), stamp=startTime, game=game, author=author, start=startTimes, dmChar=dmChar, embed=stampEmbed, embedMsg=stampEmbedmsg)
                 except discord.errors.DiscordServerError:
                     print("discord server error recorded")
                     pass
